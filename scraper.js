fd368e68f5a71af8708fbf64bdf142c6ea0f3a1c
caleb7hayes <55815986+caleb7hayes@users.noreply.github.com>
2021-11-13 14:21:04 -0600

    configuration for several resorts

--- scraper.js
+++ scraper.js
@@ -30,8 +30,132 @@ async function main() {
     }
     })
     console.log(scrapedData)
-    
 }
+
+// async function main() {
+//     const result = await request.get("https://www.showdownmontana.com/lift-tickets")
+//     const $ = cheerio.load(result)
+//     const scrapedData = []
+//     const tableHeaders = []
+//     $("section > div > div > div > div > div > div > div > div > div > div > table > tbody > tr").each((index, element) => {
+//     if (index === 0) {
+//         const ths = $(element).find("td")
+//         $(ths).each((i, element) => {
+//         tableHeaders.push(
+//             replaceAll($(element).text(), '\n', '')
+//         )
+//         })
+//     } else {
+//         const tds = $(element).find("td")
+//         const tableRow = {}
+//         $(tds).each((i, element) => {
+//             tableRow[tableHeaders[i]] = replaceAll($(element).text(), '\n','')
+//         })
+//         scrapedData.push(tableRow)
+//     }
+//     })
+//     console.log(scrapedData)
+// }
+
+// async function main() {
+//     const result = await request.get("https://losttrail.com/lift-tickets/")
+//     const $ = cheerio.load(result)
+//     const scrapedData = []
+//     const tableHeaders = []
+//     $("div > div > div > div > table > tbody > tr").each((index, element) => {
+//     if (index === 0) {
+//         const ths = $(element).find("td")
+//         $(ths).each((i, element) => {
+//         tableHeaders.push(
+//             replaceAll($(element).text(), '\n', '')
+//         )
+//         })
+//     } else {
+//         const tds = $(element).find("td")
+//         const tableRow = {}
+//         $(tds).each((i, element) => {
+//             tableRow[tableHeaders[i]] = replaceAll($(element).text(), '\n','')
+//         })
+//         scrapedData.push(tableRow)
+//     }
+//     })
+//     console.log(scrapedData)
+// }
+
+
+// async function main() {
+//     const result = await request.get("https://www.skigd.com/lift-tickets/")
+//     const $ = cheerio.load(result)
+//     const scrapedData = []
+//     const tableHeaders = []
+//     $("table > thead > tr").each((index, element) => {
+//     const ths = $(element).find("th")
+//         $(ths).each((i, element) => {
+//         tableHeaders.push(
+//             replaceAll($(element).text(), '\n', '')
+//         )
+//         })
+//     })
+//     $("table > tbody > tr").each((index, element) => {
+//         const tds = $(element).find("td")
+//         const tableRow = {}
+//         $(tds).each((i, element) => {
+//             tableRow[tableHeaders[i]] = replaceAll($(element).text(), '\n','')
+//         })
+//         scrapedData.push(tableRow)
+//     })
+//     console.log(scrapedData)
+// }
+
+// async function main() {
+//     const result = await request.get("https://www.montanasnowbowl.com/lift-tickets/")
+//     const $ = cheerio.load(result)
+//     const scrapedData = []
+//     const tableHeaders = []
+//     $("table > thead > tr").each((index, element) => {
+//     const ths = $(element).find("th")
+//         $(ths).each((i, element) => {
+//         tableHeaders.push(
+//             replaceAll($(element).text(), '\n', '')
+//         )
+//         })
+//     })
+//     $("table > tbody > tr").each((index, element) => {
+//         const tds = $(element).find("td")
+//         const tableRow = {}
+//         $(tds).each((i, element) => {
+//             tableRow[tableHeaders[i]] = replaceAll($(element).text(), '\n','')
+//         })
+//         scrapedData.push(tableRow)
+//     })
+//     console.log(scrapedData)
+// }
+
+// async function main() {
+//     const result = await request.get("https://skimaverick.com/")
+//     const $ = cheerio.load(result)
+//     const scrapedData = []
+//     const tableHeaders = []
+//     $("div > div > table > tbody > tr").each((index, element) => {
+//     if (index === 0) {
+//         const ths = $(element).find("td")
+//         $(ths).each((i, element) => {
+//         tableHeaders.push(
+//             replaceAll($(element).text(), '\n', '')
+//         )
+//         })
+//     } else {
+//         const tds = $(element).find("td")
+//         const tableRow = {}
+//         $(tds).each((i, element) => {
+//             tableRow[tableHeaders[i]] = replaceAll($(element).text(), '\n','')
+//         })
+//         scrapedData.push(tableRow)
+//     }
+//     })
+//     console.log(scrapedData)
+// }
+
 
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
