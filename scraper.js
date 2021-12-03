6d0f2fc79b0bba83733695a616b2151ce386941b
caleb7hayes <55815986+caleb7hayes@users.noreply.github.com>
2021-12-02 18:57:11 -0700

    Revert "Console log for demonstration"
    This reverts commit 7e5edf509c4bb822465a8e074bc73b7004cef1d7.

--- scraper.js
+++ scraper.js
@@ -34,7 +34,6 @@ async function whitefish() {
             scrapedData.push(tableRow)
         }
     })
-    console.log(scrapedData)
 
     // Construct a document                                                                                                                                                              
     let resortDocument = {
